068b7f923fe69131b8d1af2724cd9e51a5c343c2
barteksyko <99457989+barteksyko@users.noreply.github.com>
2022-05-25 07:52:26 +0200

    Branch (#2)
    * serialize/deserialize
    
    * changed classes: 'Main' and 'Student'

--- src/Main.cs
+++ src/Main.cs
@@ -3,23 +3,61 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace WindowsFormsApp1
 {
     public partial class Main : Form
     {
+        private string _filePath = Path.Combine(Environment.CurrentDirectory, "student.txt");
+
         public Main()
         {
             InitializeComponent();
+
+            var students = DeserializeFromFile();
+
+            dgvDiary.DataSource = students;
+        }
+
+        public void SerializeToFile(List<Student> students)
+        {
+            var serializer = new XmlSerializer(typeof(List<Student>));
+            
+            using (var streamWriter = new StreamWriter(_filePath))
+            {
+                serializer.Serialize(streamWriter, students);
+                streamWriter.Close();
+            }
+        }
+
+        public List<Student> DeserializeFromFile()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return new List<Student>();
+            }
+
+            var serializer = new XmlSerializer(typeof(List<Student>));
+
+            using (var streamReader = new StreamReader(_filePath))
+            {
+                var students = (List<Student>)serializer.Deserialize(streamReader);
+                streamReader.Close();
+                return students;
+            }
+            
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-
+            var addEditStudent = new AddEditStudent();
+            addEditStudent.ShowDialog();
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
